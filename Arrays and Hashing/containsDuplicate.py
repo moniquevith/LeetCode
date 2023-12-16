4b5aae9fc88dd89bfdf7ea9d998b9293389915fa
moniquevith <moniquevith1@gmail.com>
2023-12-16 22:31:53 +1100

    reattemped q

--- Arrays and Hashing/containsDuplicate.py
+++ Arrays and Hashing/containsDuplicate.py
@@ -2,24 +2,35 @@
 # return true if any value appears at least twice in the array, 
 # and return false if every element is distinct.
 
-def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
+# def containsDuplicate(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: bool
+#         """
         
-        # hashing: 
-        # time: O(n)
-        # space: O(n)
+#         # hashing: 
+#         # time: O(n)
+#         # space: O(n)
 
-        # if we used arrays: 
-        # time: O(n^2)
-        # space: O(1)
-        hashset = set()
+#         # if we used arrays: 
+#         # time: O(n^2)
+#         # space: O(1)
+#         hashset = set()
 
-        for n in nums: 
-            if n in hashset:
-                return True
-            hashset.add(n)
+#         for n in nums: 
+#             if n in hashset:
+#                 return True
+#             hashset.add(n)
         
-        return False
+#         return False
+
+# reattempt question 1 month later: 
+def containsDuplicate(nums):
+    hashset = {}
+    for n in nums: 
+        hashset[n] = hashset.get(n, 0) + 1
+        if hashset[n] > 1:
+            return True
+    return False
+
+containsDuplicate([1,1,1,3,3,4,3,2,4,2])
