36e500ee40e6e84e133e2003e174055e67135e9c
moniquevith <moniquevith1@gmail.com>
2023-12-17 21:51:03 +1100

    reattempted q

--- Arrays and Hashing/twoSum.py
+++ Arrays and Hashing/twoSum.py
@@ -9,10 +9,18 @@ def twoSum(nums, target):
         # using hashmap: 
         # time: O(n)
         # space: O(n)
-        hashmap = {} # value:index
-        for i in range(len(nums)):
-            if ((target - nums[i]) in hashmap): # check if the difference is in the hashmap
-                return [hashmap.get((target - nums[i])), i]
-            hashmap[nums[i]] = i # if it isnt add current value
+        # hashmap = {} # value:index
+        # for i in range(len(nums)):
+        #     if ((target - nums[i]) in hashmap): # check if the difference is in the hashmap
+        #         return [hashmap.get((target - nums[i])), i]
+        #     hashmap[nums[i]] = i # if it isnt add current value
 
-twoSum([2,7,11,15,2], 4) 
+        # reattempted question
+        hashmap = {}
+        for i in range(len(nums)):
+            if ((target - nums[i]) in hashmap):
+                print([ hashmap.get(target - nums[i]), i])
+                return [ hashmap.get(target - nums[i]), i]
+            hashmap[nums[i]] = i
+            
+twoSum([2,7,11,15,2], 18) 
