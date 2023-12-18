1e4998a209e0f2db45f93b010978b0161187aac3
moniquevith <moniquevith1@gmail.com>
2023-12-18 17:50:21 +1100

    reattempted qs

--- Arrays and Hashing/groupAnagrams.py
+++ Arrays and Hashing/groupAnagrams.py
@@ -6,19 +6,45 @@ def groupAnagrams(strs):
     """
     # time: O(m*n)
     # space: O(n)
-    hashmap = {}
+    # hashmap = {}
 
-    for s in strs: 
-        count = [0] * 26 # a .. z
-        for c in s: 
-            count[ord(c) - ord("a")] += 1 # get index for letter and add 1 (count how many times that letter appears)
+    # for s in strs: 
+    #     count = [0] * 26 # a .. z
+    #     for c in s: 
+    #         count[ord(c) - ord("a")] += 1 # get index for letter and add 1 (count how many times that letter appears)
 
-        key = tuple(count) # lists cannot be keys
-        if key in hashmap:
-            hashmap[key].append(s) # count: [str, str..]
-        else:
-            hashmap[key] = [s]
+    #     key = tuple(count) # lists cannot be keys
+    #     if key in hashmap:
+    #         hashmap[key].append(s) # count: [str, str..]
+    #     else:
+    #         hashmap[key] = [s]
             
-    return list(hashmap.values())
+    # return list(hashmap.values())
+
+    # reattempt question
+    hashmap = {}
+    '''
+        {
+            eat: ["eat", "tea", "ate"]
+        }
+    '''
+    # same length and same letters 
+    for s in strs:
+        temp = ''.join(sorted(s))
+        if temp in hashmap:
+            new_list = hashmap.get(temp) + [s]
+            hashmap[temp] = new_list
+            continue
+        hashmap[temp] = [s]
+    
+    result = []
+
+    if hashmap == {}:
+        result = [[""]]
+
+    for key in hashmap:
+        result.append(hashmap.get(key))
+
+    return result
 
 groupAnagrams(["eat","tea","tan","ate","nat","bat"])
