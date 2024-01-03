810f79a757b277130d524970e2cf7b0142df918f
moniquevith <moniquevith1@gmail.com>
2024-01-03 21:23:10 +1100

    finally figured out how to check valid square in grid

--- Arrays and Hashing/validSudoku.py
+++ Arrays and Hashing/validSudoku.py
@@ -4,51 +4,46 @@ def isValidSudoku(board):
     :rtype: bool
     """
     # check if rows have no reps 
-    # row_digits = {}
-    # for row in range(9):
-    #     for col in range(9):
-    #         if board[row][col] == ".":
-    #             continue
-    #         else: 
-    #             if board[row][col] in row_digits:
-    #                 print("not valid row")
-    #             else: 
-    #                 row_digits[board[row][col]] = 1
-    #     row_digits = {}
+    row_digits = {}
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == ".":
+                continue
+            else: 
+                if board[row][col] in row_digits:
+                    print("not valid row")
+                else: 
+                    row_digits[board[row][col]] = 1
+        row_digits = {}
 
-    # # check if cols have no reps 
-    # column_digits = {}
-    # for col in range(9):
-    #     for row in range(9):
-    #         if board[row][col] == ".":
-    #             continue
-    #         else: 
-    #             if board[row][col] in column_digits:
-    #                 print("not valid col")
-    #             else: 
-    #                 column_digits[board[row][col]] = 1
-    #     column_digits = {}
+    # check if cols have no reps 
+    column_digits = {}
+    for col in range(9):
+        for row in range(9):
+            if board[row][col] == ".":
+                continue
+            else: 
+                if board[row][col] in column_digits:
+                    print("not valid col")
+                else: 
+                    column_digits[board[row][col]] = 1
+        column_digits = {}
 
-    # # check if 3x3 boxes have reps 
-    # box_digits = {}
-    # for row in range(9):
-    #     for col in range(9):
-    #         return
-    
-    # cols = {c: set() for c in range(9)}
-    # for r in range(9):
-    #     for c in range(9):
-    #         cols[c].add(board[r][c])
-    # print(cols)
+    # check if 3x3 boxes have reps 
+    box_digits = {} # {(0,0): [1,..,9], (0,1): [1,..,9] }
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == ".":
+                continue
+            else:
+                if (row // 3, col // 3) in box_digits and board[row][col] in box_digits[(row // 3, col // 3)]: # check if the key pair value exists and if the current value is in the list
+                    print("not valid square")
+                else: # if current value not already in list, add it
+                    new_list = box_digits.get((row // 3, col // 3), []) + [board[row][col]] # add the current value to the list of that square, if that key doesnt exist make it equal to []
+                    box_digits[(row // 3, col // 3)] = new_list # change that key pair value to the new list 
 
-board = [["5","3",".",".","7",".",".",".","."]
-,["6",".",".","1","9","5",".",".","."]
-,[".","9","8",".",".",".",".","6","."]
-,["8",".",".",".","6",".",".",".","3"]
-,["4",".",".","8",".","3",".",".","1"]
-,["7",".",".",".","2",".",".",".","6"]
-,[".","6",".",".",".",".","2","8","."]
-,[".",".",".","4","1","9",".",".","5"]
-,[".",".",".",".","8",".",".","7","9"]]
+    return True
+
+board = [[".",".",".",".","5",".",".","1","."],[".","4",".","3",".",".",".",".","."],[".",".",".",".",".","3",".",".","1"],["8",".",".",".",".",".",".","2","."],[".",".","2",".","7",".",".",".","."],[".","1","5",".",".",".",".",".","."],[".",".",".",".",".","2",".",".","."],[".","2",".","9",".",".",".",".","."],[".",".","4",".",".",".",".",".","."]]
 
 isValidSudoku(board)
