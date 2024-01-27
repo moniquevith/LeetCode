cccdbd6e1adaaf8de217b5872f2505918ad33ac1
moniquevith <moniquevith1@gmail.com>
2024-01-27 11:42:22 +1100

    completed q

--- Two Pointers/twoSumII.py
+++ Two Pointers/twoSumII.py
@@ -5,14 +5,15 @@ def twoSum(numbers, target):
     :rtype: List[int]
     """
     pointer_1 = 0
-    pointer_2 = pointer_1 + 1
-    while pointer_1 != len(numbers):
-        for idx in range(pointer_2, len(numbers), 1):
-            if (numbers[pointer_1] + numbers[idx] == target):
-                return [pointer_1 + 1, idx + 1]
-        pointer_1 += 1
-        pointer_2 += 1
-
+    pointer_2 = len(numbers) - 1
+    while pointer_1 < pointer_2:
+        if ((numbers[pointer_1] + numbers[pointer_2]) > target):
+            pointer_2 -= 1
+        if ((numbers[pointer_1] + numbers[pointer_2]) < target):
+            pointer_1 += 1
+        if ((numbers[pointer_1] + numbers[pointer_2]) == target):
+            return [pointer_1 + 1, pointer_2 + 1]
+        
 numbers = [1,2,11,15]
 target = 13
 twoSum(numbers, target)
