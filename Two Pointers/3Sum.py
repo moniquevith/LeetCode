43a4b5ed77fad048c295420fa6e7e705f2ce9c72
Monique Vith <z5310404@ad.unsw.edu.au>
2024-02-01 20:01:18 +1100

    havent removed duplicates

--- Two Pointers/3Sum.py
+++ Two Pointers/3Sum.py
@@ -4,16 +4,20 @@ def threeSum(nums):
     :rtype: List[List[int]]
     """
     results = []
-    hashmap = {}
-    for idx in range(len(nums)): # 0 - 5
-        target = - nums[idx] # 1
-        pointer1 = idx + 1 
+    nums.sort()
+    for n in range(len(nums)):
+        target = - nums[n]
+        pointer1 = n + 1
         pointer2 = len(nums) - 1
-        while (pointer1 < pointer2):
-            if (target - nums[pointer1] in hashmap):
-                results.append([-target, nums[pointer1], hashmap.get(target - nums[pointer1])])
-            hashmap[nums[pointer1]] = nums[pointer1]
-            pointer1 += 1
+        while (pointer2 > pointer1):
+            if (nums[pointer1] + nums[pointer2]) > target:
+                pointer2 -= 1
+            elif (nums[pointer1] + nums[pointer2]) < target:
+                pointer1 += 1
+            else:
+                results.append([-target, nums[pointer1], nums[pointer2]])
+                break
+    return results
 
 nums = [-1,0,1,2,-1,-4]
 threeSum(nums)
