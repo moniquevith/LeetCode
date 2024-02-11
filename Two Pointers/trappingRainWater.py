53681ea4c83c8d6656b77536f68c65f557dcb082
moniquevith <moniquevith1@gmail.com>
2024-02-11 15:47:52 +1100

    i think i got it working???

--- Two Pointers/trappingRainWater.py
+++ Two Pointers/trappingRainWater.py
@@ -6,19 +6,23 @@ def trap(height):
 
     pointer1 = 0
     pointer2 = pointer1 + 1
-    while (pointer1 <= pointer2):
-        if height[pointer2] >= height[pointer1] and pointer2 != pointer1 + 1: # check for next largest height and pointer2 is not next to pointer1
-            h = min(height[pointer1], height[pointer2]):
+    output = 0
+    while (pointer1 != (len(height) - 1)):
+        if height[pointer2] >= height[pointer1]: # check for next largest height
+            h = min(height[pointer1], height[pointer2])
             w = pointer2 - pointer1 - 1
             blocks_inbetween = 0
-            for idx in range(pointer1, pointer2, 1):
+            for idx in range(pointer1, pointer2):
                 if idx == pointer1 or idx == pointer2:
                     continue
                 blocks_inbetween += height[idx]
             v = h * w - blocks_inbetween
-        pointer1 = pointer2
-        pointer2 = pointer1 + 1
-
-height = [0,1,0,2,1,0,1,3,2,1,2,1]
+            output += v
+            pointer1 = pointer2
+            pointer2 += 1
+        pointer2 += 1
+    print(output)
+height = [0, 1, 0, 2, 1, 0, 1, 3]
+# height = [0,1,0,2,1,0,1,3,2,1,2,1]
 # expected output = 6
 trap(height)
